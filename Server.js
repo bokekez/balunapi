2d8d09abb6fe44a1d8accdf4fe2c03bf92e2415f
bokekez <bokekez@gmail.com>
2021-06-26 22:50:47 +0200

    mongo route / unos

--- Server.js
+++ Server.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
-const Termin = require('./Models/termini')
+const Termin = require('./Models/termini');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,19 +13,33 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected'))
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) =>{
-    const tempTermin = Termin.find();
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
+
+app.get('/', cors(), async (req, res) =>{
+    const tempTermin = await Termin.find();
+    // const totalTermin = tempTermin.map(element => ({
+    //     // 'totalG' = element.igrač1G+element.igrač2G+element.igrač3G+element.igrač4G+element.igrač5G+element.igrač6G+element.igrač7G+element.igrač8G
+    //     'totalG': element.igrač1G +element.igrač2G,
+    // }));
+    console.log('1', tempTermin)
     res.json(tempTermin)
 })
 
-app.post('/unos', (req, res) => {
+app.post('/unos', async (req, res) => {
     const tempTermin = new Termin({
         igrač1: req.body.igrač1,
-        igrač1G: req.body.igrač1G
+        igrač1G: req.body.igrač1G,
+        igrač2: req.body.igrač2,
+        igrač2G: req.body.igrač2G
     })
     try {
-        const newTermin = tempTermin.save();
+        const newTermin = await tempTermin.save();
         res.status(201).json(newTermin)
     }
     catch(err){
